b56c80db91ae3d0f7b5976cd466bc811058d4816
Yusef Al-Naher <47220381+YusefAN@users.noreply.github.com>
2022-04-24 01:39:05 +0200

    Finished Contract

--- contracts/LendNFT.sol
+++ contracts/LendNFT.sol
@@ -1,3 +1,4 @@
+
 //SPDX-License-Identifier: Unlicense
 pragma solidity 0.8.1;
 
@@ -8,10 +9,13 @@ contract LendNft is IERC721Receiver {
     // Contract will be defined by two different structures: 
     // loanOffer  defines parameters that the lender sets with respective NFT info
     // loan defines the loan that's taken along with it's state
-
     // loanOffer and loan both accessed through a mapping from loanOfferId and loanID  
-
     // Structure for Loan Offer
+    event loanCreated(address _loanCreator, address _nftAddress, uint NftID, uint _loanFee, uint _colateralAmount,  uint _lengthInDays);
+    
+    address owner;
+    mapping(uint => bool) loanActive;
+
     struct _loanOffer {
         bool active;
         address nftContract;
@@ -23,19 +27,28 @@ contract LendNft is IERC721Receiver {
         uint loanExpiry;
         uint offerTime;
         }
+    
 
     struct _loan {
-    bool active;
-    uint loanOfferID;
-    _loanOffer loanOffer;
-    address borrower;
-    uint borrowTime;
-    uint returnTime;
+        bool active;
+        uint loanOfferID;
+        _loanOffer loanOffer;
+        address borrower;
+        uint borrowTime;
+        uint returnTime;
+        address _lender;
+        uint loanCollateral;
+    }
+
+    constructor() {
+        owner = msg.sender;
     }
 
     // loanOfferID and loanID that starts at 0. ++ each time a new offer is put up
     uint public loanOfferID;
     uint public loanID;
+
+    uint public timeMultiplier=1;
 
     // Mappings from loanOfferID to respective loan
     mapping(uint => _loanOffer) public loanOffers;
@@ -44,99 +57,70 @@ contract LendNft is IERC721Receiver {
     // Array of activeOffers - getter function will return an array of activeOfferIDs. 
     // Getter function is getActiveOffers(). Needs to updated with updateActiveOffers()
     uint[] public activeOffers;
-
-    function createLoanOffer(address _nft, uint _id, address _lender, uint _fee, uint _collateral, uint _length, uint _expiry) public payable { 
-        //Requires NFT approval  
+    
+    function createLoanOffer(address _nft, uint _id, uint _fee, uint _collateral, uint _lengthInDays ) public payable { 
+        //Requires NFT approval
         IERC721(_nft).transferFrom(msg.sender, address(this), _id);
-        loanOffers[loanOfferID]=_loanOffer(true, _nft, _id,_lender, _fee, _collateral, _length, _expiry, block.timestamp); 
+        loanOffers[loanOfferID]=_loanOffer(true, _nft, _id, msg.sender , _fee, _collateral, _lengthInDays, block.timestamp + (_lengthInDays * timeMultiplier) , block.timestamp);
+        
+        emit loanCreated(msg.sender,_nft,_id,_fee,_collateral,_lengthInDays);
+        
         //receive NFT 
         loanOfferID++;
     }
 
-    //_id=_loanOfferID
+
+
     function cancelLoanOffer(uint _loanOfferID) public payable { 
         //Needs that the sender is loanoffer owner and that it's still active
-        require(loanOffers[_loanOfferID].lender==msg.sender && loanOffers[_loanOfferID].active==true);
-
+        require(loanOffers[_loanOfferID].lender==msg.sender,"not owner of loan");
+        require(loanOffers[_loanOfferID].active==true,"loan not active");
         //Transfer NFT from contract address to message sender
-        IERC721(loanOffers[_loanOfferID].nftContract).transferFrom(address(this),msg.sender, loanOffers[_loanOfferID].nftID);
-        
         loanOffers[_loanOfferID].active=false;
+        IERC721(loanOffers[_loanOfferID].nftContract).transferFrom(address(this),msg.sender, loanOffers[_loanOfferID].nftID);
     }
 
-    // Function to update the activeOffers array that returns the loanIDs
-    function updateActiveOffers() public { 
-        delete activeOffers;
-        for(uint i=0; i < loanOfferID; i++) { 
-            require(loanOffers[i].active==true); 
-            activeOffers.push(i);
-        }
-    }
-
-    // Getter function for active offers
-    function getAllActiveOffers() public view returns(uint[] memory) { 
-        return activeOffers;
-    }
 
 
     function takeLoan(uint _loanOfferID) public payable { 
-        require(msg.value == loanOffers[_loanOfferID].loanCollateral + loanOffers[_loanOfferID].loanFee);
+        require(msg.value == loanOffers[_loanOfferID].loanCollateral + loanOffers[_loanOfferID].loanFee,"Did not send enough Colat+fee");
+        require(loanActive[loanID] == false,"loan already Active!");
         //Send NFT 
         IERC721(loanOffers[_loanOfferID].nftContract).transferFrom(address(this),msg.sender, loanOffers[_loanOfferID].nftID);
         //Make loanOffer active = F 
         loanOffers[loanID].active=false;
 
         //Add active loan
-        loans[loanID]=_loan(true, _loanOfferID, loanOffers[_loanOfferID], msg.sender, block.timestamp, block.timestamp + loanOffers[loanID].loanLength);
+        loanActive[loanID] = true;
+        loans[loanID]=_loan(true, _loanOfferID, loanOffers[_loanOfferID], msg.sender, block.timestamp, block.timestamp + (loanOffers[_loanOfferID].loanLength * timeMultiplier),loanOffers[loanID].lender, loanOffers[loanID].loanCollateral);
         loanID++;
-        updateActiveOffers();
+
     }
 
-    //This fn doesnt work
-    // function getActiveOffers(address _address) public view returns(uint[] memory) { 
-    //     uint[] memory allOffers=getAllActiveOffers();
-    //     uint[] memory specific;
-    //     uint j;
-    //     for(uint i=0; i<allOffers.length; i++) { 
-    //         if(loanOffers[ allOffers[i] ].lender==_address) { 
-    //             specific[j] = allOffers[i];
-    //             j++;
-    //         }
-    //         else { 
-    //             j++;
-    //         }
-    //     }
-    //     return specific;
-    // }
 
-    //  function InitLoan() payable {
-    //     require(msg.value >= _colat + _loanAmount,"not enough colateral");
-        // require requested loanlength < max loan length
-        // record loan details
-        // transfer the NFT
-        // start timer
-        // takes cut from LoanFee
-        // pay remainder LoanFee to lender
-  //  }
+    function liquidateLoan() external payable {
+        for(uint i; i<loanID; i++) { 
+            //require(msg.sender == owner,"not owner"); < Do we need owner here? 
+            require(loanActive[i] == true,"loan not active");
+            require(block.timestamp > loans[i].returnTime,"not time to liquidate");
+            loanActive[i] = false;
+            loans[i].active=false;
+            payable(loans[i]._lender).transfer(loans[i].loanCollateral);
+        }
+    }
 
-    // function createLoan(address lender, uint _colat, uint _loanAmount, uint maxLoanLength) public payable {
-    //     loanOffer memory loan = loanOffer(_nft)
-    // }
+    function repayLoan(uint _loanID) public payable {
+       require(loans[_loanID].active==true);
+       require(loans[_loanID].borrower==msg.sender);
+       //Receive NFT from msg sender
+       IERC721(loans[_loanID].loanOffer.nftContract).transferFrom(msg.sender, address(this), loans[_loanID].loanOffer.nftID);
+       
+       //Repay the collateral
+       payable(loans[_loanID]._lender).transfer(loans[_loanID].loanCollateral);
 
-
-
-    // function liquidateLoan() external {
-    //     // close loan
-    // }
-
-    // function repayLoan() payable {
-    //     // mark complete
-    //     // receives the NFT
-    //     // returns collateral
-    // }
-
-    // // can we farm collateral?
-    // // 
+       //Send NFT back to owner
+       IERC721(loans[_loanID].loanOffer.nftContract).transferFrom(address(this), loanOffers[_loanID].lender , loans[_loanID].loanOffer.nftID);
+    }
 
     
     // Receive ERC721 
--- test/tests.js
+++ test/tests.js
@@ -8,6 +8,8 @@ describe("LendNft", function () {
   let nftm; 
   let nftid;
   let acc_add;
+  let acc_other;
+  let loansactive=[];
 
   beforeEach(async() => {
     const Lending = await ethers.getContractFactory("LendNft");
@@ -16,13 +18,12 @@ describe("LendNft", function () {
     nftm = await Nftm.deploy();
     lending = await Lending.deploy();
 
-    acc_add='0x34Ad68800452563374f800932148433e83632fee';
+    //acc_add='0x34Ad68800452563374f800932148433e83632fee';
     nftid=1;
-    // await lending.deployed();
-    console.log(lending.address);
-
-    const [deployer] = await ethers.getSigners();
+    
+    [deployer, acc_other] = await ethers.getSigners();
     acc_add=deployer.address;
+    acc_other=acc_other.address;
 
   console.log("Deploying contracts with the account:", deployer.address);
   });
@@ -30,6 +31,7 @@ describe("LendNft", function () {
     
     await nftm.safeMint(lending.address, 0);
     // Send NFT - get balance 
+    console.log()
     expect(await nftm.balanceOf(lending.address)).to.equal(1);
   });
 
@@ -37,18 +39,22 @@ describe("LendNft", function () {
     //Mint + Approve
     await nftm.safeMint(acc_add, nftid);
     expect(await nftm.ownerOf(nftid)).to.equal(acc_add);
-    console.log('NFT Minted and Owner should be account' + await nftm.ownerOf(nftid));
     
+
+
     //Lend out NFT
     await nftm.approve(lending.address, nftid);
-    await lending.createLoanOffer(nftm.address, nftid, acc_add,1,10, 20, 100);    
+    await lending.createLoanOffer(nftm.address, nftid,10, 20, 100);    
+    
     expect(await nftm.ownerOf(nftid)).to.equal(lending.address);
-    console.log('NFT lent out and owner should be ' + await nftm.ownerOf(nftid));
+    console.log('NFT Owner:' + await nftm.ownerOf(nftid));
+    console.log('Contract Address:' + lending.address);
     
     //Cancel the offer
     await lending.cancelLoanOffer(0);
     expect(await nftm.ownerOf(nftid)).to.equal(acc_add);
-    console.log('NFT lend out cancelled and now NFT should be returned ' + await nftm.ownerOf(nftid));
+    console.log('NFT Owner:' + await nftm.ownerOf(nftid));
+    console.log('Contract Address:' + lending.address);
 
   });
 
@@ -60,10 +66,9 @@ describe("LendNft", function () {
     
     //Lend out NFT
     await nftm.approve(lending.address, nftid);
-    await lending.createLoanOffer(nftm.address, nftid, acc_add,1,10, 20, 100);    
+    await lending.createLoanOffer(nftm.address, nftid,10, 20, 100);    
     expect(await nftm.ownerOf(nftid)).to.equal(lending.address);
     console.log('NFT lent out and owner should be ' + await nftm.ownerOf(nftid));
-
 
   });
 
@@ -75,11 +80,82 @@ describe("LendNft", function () {
     
     //Lend out NFT
     await nftm.approve(lending.address, nftid);
-    await lending.createLoanOffer(nftm.address, nftid, acc_add,1,10, 20, 100);    
+    await lending.createLoanOffer(nftm.address, nftid,10, 20, 100);    
     expect(await nftm.ownerOf(nftid)).to.equal(lending.address);
     console.log('NFT lent to protocol and owner should be ' + await nftm.ownerOf(nftid));
 
+        
+  });
 
+  it("Should be able to match the loan offer", async function () {
+    //Mint + Approve
+    await nftm.safeMint(acc_add, nftid);
+    expect(await nftm.ownerOf(nftid)).to.equal(acc_add);
+    console.log('NFT Minted and Owner should be account' + await nftm.ownerOf(nftid));
+    
+    //Lend out NFT
+    await nftm.approve(lending.address, nftid);
+    await lending.createLoanOffer(nftm.address, nftid,10, 20, 100);    
+    await lending.takeLoan(0, {value: 30});
+    
+
+        
+  });
+
+  // it("Should be able to match the loan offer", async function () {
+  //   //Mint + Approve
+  //   for(let i=0; i<10; i++) { 
+  //   await nftm.safeMint(acc_add, nftid);
+  //   expect(await nftm.ownerOf(nftid)).to.equal(acc_add);
+  //   console.log('NFT Minted and Owner should be account' + await nftm.ownerOf(nftid));
+    
+  //   //Lend out NFT
+  //   await nftm.approve(lending.address, nftid);
+  //   await lending.createLoanOffer(nftm.address, nftid,10, 20, 100);    
+  //   await lending.takeLoan(i, {value: 30});
+  //   nftid++;
+  // }
+
+  // loansactive=[]
+  // for(let i=1; i<10; i++) {
+  //   x=await lending.loans(i)  
+  //   loansactive.push(x.active);
+  // }
+
+  // console.log(loansactive);
+  // await lending.liquidateLoan();
+  // await new Promise(r => setTimeout(r, 2000));
+  
+  // loansactive=[]
+  // for(let i=1; i<10; i++) {
+  //   x=await lending.loans(i) 
+    
+  //   loansactive.push(x.active);
+  // }
+
+  // console.log(loansactive);
+        
+  // });
+
+  it("Should be able to repay the loan offer", async function () {
+    //Mint + Approve
+    await nftm.safeMint(acc_add, nftid);
+    expect(await nftm.ownerOf(nftid)).to.equal(acc_add);
+    console.log('NFT Minted and Owner should be account' + await nftm.ownerOf(nftid));
+    
+    //Lend out NFT
+    await nftm.approve(lending.address, nftid);
+    await lending.createLoanOffer(nftm.address, nftid,10, 20, 100);    
+
+    console.log('NFT loaned and should be contract' + await nftm.ownerOf(nftid));
+    await lending.takeLoan(0, {value: 30});
+    console.log('NFT borrowed and should be account' + await nftm.ownerOf(nftid));
+
+    await nftm.approve(lending.address, nftid);
+    await lending.repayLoan(0);
+    console.log('NFT repaid and should be account' + await nftm.ownerOf(nftid));
+
+        
   });
 
 });
